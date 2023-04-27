84f36dba99a4cb7c7050762a67180a72204845d9
Dulon18 <dulon587@gmail.com>
2023-04-27 23:21:10 +0600

    added css file

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -5,23 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard Admin Template by Tooplate.com</title>
+    <title>Dashboard Admin</title>
     <!--
 
     Template 2108 Dashboard
 
-	http://www.tooplate.com/view/2108-dashboard
+ http://www.tooplate.com/view/2108-dashboard
 
     -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600">
     <!-- https://fonts.google.com/specimen/Open+Sans -->
-    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="{{ url('backend/css/fontawesome.min.css') }}">
     <!-- https://fontawesome.com/ -->
-    <link rel="stylesheet" href="css/fullcalendar.min.css">
+    <link rel="stylesheet" href="{{ url('backend/css/fullcalendar.min.css') }}">
     <!-- https://fullcalendar.io/ -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ url('backend/css/bootstrap.min.css') }}">
     <!-- https://getbootstrap.com/ -->
-    <link rel="stylesheet" href="css/tooplate.css">
+    <link rel="stylesheet" href="{{ url('backend/css/tooplate.css') }}">
 </head>
 
 <body id="reportsPage">
@@ -34,7 +34,8 @@
                             <i class="fas fa-3x fa-tachometer-alt tm-site-icon"></i>
                             <h1 class="tm-site-title mb-0">Dashboard</h1>
                         </a>
-                        <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -47,7 +48,8 @@
                                     </a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true"
                                         aria-expanded="false">
                                         Reports
                                     </a>
@@ -65,7 +67,8 @@
                                     <a class="nav-link" href="accounts.html">Accounts</a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true"
                                         aria-expanded="false">
                                         Settings
                                     </a>
@@ -180,7 +183,8 @@
                 <div class="col-12 font-weight-light">
                     <p class="d-inline-block tm-bg-black text-white py-2 px-4">
                         Copyright &copy; 2018 Admin Dashboard . Created by
-                        <a rel="nofollow" href="https://www.tooplate.com" class="text-white tm-footer-link">Tooplate</a>
+                        <a rel="nofollow" href="https://www.tooplate.com"
+                            class="text-white tm-footer-link">Tooplate</a>
                     </p>
                 </div>
             </footer>
@@ -211,14 +215,14 @@
             lineChart;
         barChart, pieChart;
         // DOM is ready
-        $(function () {
+        $(function() {
             updateChartOptions();
             drawLineChart(); // Line Chart
             drawBarChart(); // Bar Chart
             drawPieChart(); // Pie Chart
             drawCalendar(); // Calendar
 
-            $(window).resize(function () {
+            $(window).resize(function() {
                 updateChartOptions();
                 updateLineChart();
                 updateBarChart();
@@ -227,4 +231,5 @@
         })
     </script>
 </body>
+
 </html>
