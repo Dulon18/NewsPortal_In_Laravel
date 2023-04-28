7be9e7871c44b9c3f82f9a6accaa0569627184d2
Dulon18 <dulon587@gmail.com>
2023-04-28 23:51:55 +0600

    mastering admin templete

---
+++ resources/views/backend/fixed/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="row tm-mt-small">
+    <div class="col-12 font-weight-light">
+        <p class="d-inline-block tm-bg-black text-white py-2 px-4">
+            Copyright &copy; News Potal Admin Dashboard . Created by
+            <a rel="nofollow" href="https://www.tooplate.com" class="text-white tm-footer-link">Tooplate</a>
+        </p>
+    </div>
+</footer>
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -29,66 +29,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <nav class="navbar navbar-expand-xl navbar-light bg-light">
-                        <a class="navbar-brand" href="#">
-                            <i class="fas fa-3x fa-tachometer-alt tm-site-icon"></i>
-                            <h1 class="tm-site-title mb-0">Dashboard</h1>
-                        </a>
-                        <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mx-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#">Dashboard
-                                        <span class="sr-only">(current)</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">
-                                        Reports
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Daily Report</a>
-                                        <a class="dropdown-item" href="#">Weekly Report</a>
-                                        <a class="dropdown-item" href="#">Yearly Report</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="products.html">Products</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="accounts.html">Accounts</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">
-                                        Settings
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Profile</a>
-                                        <a class="dropdown-item" href="#">Billing</a>
-                                        <a class="dropdown-item" href="#">Customize</a>
-                                    </div>
-                                </li>
-                            </ul>
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link d-flex" href="login.html">
-                                        <i class="far fa-user mr-2 tm-logout-icon"></i>
-                                        <span>Logout</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+                    @include('backend.fixed.nav')
                 </div>
             </div>
             <!-- row -->
@@ -179,29 +120,21 @@
                     </div>
                 </div>
             </div>
-            <footer class="row tm-mt-small">
-                <div class="col-12 font-weight-light">
-                    <p class="d-inline-block tm-bg-black text-white py-2 px-4">
-                        Copyright &copy; 2018 Admin Dashboard . Created by
-                        <a rel="nofollow" href="https://www.tooplate.com"
-                            class="text-white tm-footer-link">Tooplate</a>
-                    </p>
-                </div>
-            </footer>
+            @include('backend.fixed.footer')
         </div>
     </div>
-    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="{{ url('backend/js/jquery-3.3.1.min.js') }}"></script>
     <!-- https://jquery.com/download/ -->
-    <script src="js/moment.min.js"></script>
+    <script src="{{ url('backend/js/moment.min.js') }}"></script>
     <!-- https://momentjs.com/ -->
-    <script src="js/utils.js"></script>
-    <script src="js/Chart.min.js"></script>
+    <script src="{{ url('backend/js/utils.js') }}"></script>
+    <script src="{{ url('backend/js/Chart.min.js') }}"></script>
     <!-- http://www.chartjs.org/docs/latest/ -->
-    <script src="js/fullcalendar.min.js"></script>
+    <script src="{{ url('backend/js/fullcalendar.min.js') }}"></script>
     <!-- https://fullcalendar.io/ -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="{{ url('backend/js/bootstrap.min.js') }}"></script>
     <!-- https://getbootstrap.com/ -->
-    <script src="js/tooplate-scripts.js"></script>
+    <script src="{{ url('backend/js/tooplate-scripts.js') }}"></script>
     <script>
         let ctxLine,
             ctxBar,
