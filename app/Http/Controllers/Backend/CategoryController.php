6cf0c0ee135a47d3e429afccb6e5a0c4619c4b71
Dulon18 <dulon587@gmail.com>
2023-05-02 18:18:55 +0600

    create&store category in db

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -3,16 +3,33 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
     public function category_list()
     {
-        return view('backend.pages.category.category_list');
+        $categories=DB::table('categories')->get();
+        return view('backend.pages.category.category_list',compact('categories'));
     }
     public function create_category()
     {
         return view('backend.pages.category.create_category');
+    }
+    public function store_category(Request $request)
+    {
+        $request->validate([
+            'category_en'=>'required',
+            'category_bn'=>'required',
+
+        ]);
+        Category::create([
+            'category_en'=>$request->category_en,
+            'category_bn'=>$request->category_bn,
+        ]);
+
+        return redirect()->route('category.list');
     }
 }
--- resources/views/backend/pages/category/create_category.blade.php
+++ resources/views/backend/pages/category/create_category.blade.php
@@ -1,3 +1,39 @@
 @extends('backend.layouts.app')
 @section('content')
+    <div class="col-xl-12 col-lg-10 col-md-12 col-sm-12">
+        <div class="bg-white tm-block">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="tm-block-title d-inline-block">Add Product</h2>
+                </div>
+            </div>
+            <div class="row mt-4 tm-edit-product-row">
+                <div class="col-xl-7 col-lg-7 col-md-12">
+                    <form action="{{ route('category.store') }}" class="tm-edit-product-form">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <label for="name" class="col-xl-4 col-lg-4 col-md-4 col-sm-5 col-form-label">Category
+                                English Name
+                            </label>
+                            <input id="name" name="category_en" type="text"
+                                class="form-control validate col-xl-9 col-lg-8 col-md-8 col-sm-7">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="stock" class="col-xl-4 col-lg-4 col-md-4 col-sm-5 col-form-label">Category Bangla
+                                Name
+                            </label>
+                            <input id="stock" name="category_bn" type="text"
+                                class="form-control validate col-xl-9 col-lg-8 col-md-7 col-sm-7">
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="ml-auto col-xl-8 col-lg-8 col-md-8 col-sm-7 pl-0">
+                                <button type="submit" class="btn btn-primary">Add
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
