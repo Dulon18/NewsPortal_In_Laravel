92640bbf866f8993aeaf22afd898aeed11a0cb22
Dulon18 <dulon587@gmail.com>
2023-05-06 17:28:17 +0600

    added post migration

--- database/migrations/2023_05_06_103155_create_posts_table.php
+++ database/migrations/2023_05_06_103155_create_posts_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->integer('cat_id');
+            $table->integer('subcat_id')->nullable();
+            $table->integer('dist_id');
+            $table->integer('subdist_id')->nullable();
+            $table->string('title_bn');
+            $table->string('title_en');
+            $table->string('image');
+            $table->text('details_en')->nullable();
+            $table->text('details_bn');
+            $table->text('tags_bn');
+            $table->text('tags_en')->nullable();
+            $table->integer('headline')->nullable();
+            $table->integer('first_section')->nullable();
+            $table->integer('first_section_thumbnail')->nullable();
+            $table->integer('bigthumbnail')->nullable();
+            $table->string('post_date')->nullable();
+            $table->string('post_month')->nullable();
             $table->timestamps();
         });
     }
