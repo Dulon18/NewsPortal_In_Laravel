ee3f3a9adb5b9a4b8892f4b0a95ceff4ea16040c
Dulon18 <dulon587@gmail.com>
2023-05-18 23:59:31 +0600

    post data store

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\District;
 use App\Models\SubCategory;
 use App\Models\SubDistrict;
@@ -22,5 +23,28 @@ class PostController extends Controller
         $districts=District::all();
         $subdistricts=SubDistrict::all();
         return view('backend.pages.post.create_post',compact('categories', 'subcategories', 'districts', 'subdistricts'));
+    }
+    public function store(Request $request)
+    {
+        Post::create([
+            'title_en'=>$request->title_en,
+            'title_bn'=>$request->title_bn,
+            'cat_id'=>$request->cat_id,
+            'subcat_id'=>$request->subcat_id,
+            'dist_id'=>$request->dist_id,
+            'subdist_id'=>$request->subdist_id,
+            'details_en'=>$request->details_en,
+            'details_bn'=>$request->details_bn,
+            'tags_bn'=>$request->tags_bn,
+            'tags_en'=>$request->tags_en,
+            'headline'=>$request->headline,
+            'first_section'=>$request->first_section,
+            'first_section_thumbnail'=>$request->first_section_thumbnail,
+            'bigthumbnail'=>$request->bigthumbnail,
+            'post_date'=>$request->post_date,
+            'post_month'=>$request->post_month,
+
+        ]);
+        return redirect()->back();
     }
 }
